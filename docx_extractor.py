5c0ac9af98d647c40040ec8103ceee39c9ad3382
hkolb <hkolb@kpmgcloudops.onmicrosoft.com>
2025-11-21 14:19:33 +0000

    Initial commit

---
+++ docx_extractor.py
@@ -0,0 +1,110 @@
+import os
+import tempfile
+import pandas as pd
+from zipfile import ZipFile
+import xml.etree.ElementTree as ET
+
+namespaces = {
+    'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main',
+    'w14': 'http://schemas.microsoft.com/office/word/2010/wordml'
+}
+
+# Read document.xml
+# =================
+def get_xml_root(docx_path: str) -> ET.Element:
+    """ Extracts and reads the document.xml file from a docx and returns
+        The root of the XML tree as a xml ElementTree Element.
+
+    Args:
+        docx_path (str): _description_
+
+    Returns:
+        ET.Element: _description_
+    """
+    
+    # Create temp directory for files
+    tmpdir = tempfile.mkdtemp()
+
+    # Manipulate strings to format paths
+    filename = docx_path[docx_path.rfind("\\")+1:-5]
+    xml_subpath = f"{filename}/word/document.xml"
+    doc_xml_path = f"{tmpdir}/{xml_subpath}"
+
+    # Extract just the document.xml file
+    with ZipFile(docx_path, 'r') as zObject:
+        zObject.extractall(tmpdir, members=[xml_subpath])
+
+    # Parse document.xml
+    tree = ET.parse(doc_xml_path)
+
+    # Return root Element of parsed xml ElementTree object
+    return tree.getroot()
+
+
+# Extract data from tables
+# =======================
+def get_tabular_data(docx_path: str) -> pd.DataFrame:
+    """ Locate all data from any tables in the document.
+        This wll usually be: 
+            Name of Insured «Text»
+            Indident Date, «Date»
+            Type of incident, «Categorical Text»
+            Location, «ISO 3166 Alpha-2 Code»
+            Industry, «SIC Alphabetic Industry Class» 
+            Args:
+            docx_path (str): Path to a .docx file
+            
+    Args:
+        docx_path (str): Path to a .docx file
+        
+    Returns:
+        pd.DataFrame: Table featuring 
+    """
+    root = get_xml_root(docx_path)
+    tables = []
+
+    # Find all tables
+    for tbl in root.findall('.//w:tbl', namespaces):
+        rows = []
+        for tr in tbl.findall('.//w:tr', namespaces):
+            cells = []
+            for tc in tr.findall('.//w:tc', namespaces):
+                # Extract all text in this cell
+                texts = [t.text for t in tc.findall('.//w:t', namespaces) if t.text]
+                cell_text = ' '.join(texts).strip()
+                cells.append(cell_text)
+
+            rows.append(cells)
+
+        # Convert to DataFrame
+        df = pd.DataFrame(rows)
+        tables.append(df)
+
+    # Combine tables and return
+    return pd.concat(tables)
+
+
+# Extract text data
+# =================
+def get_text_data(docx_path: str, split_paragraphs: bool = False):
+    """ Reads raw text and paragraph data from a .docx file.
+
+    Args:
+        docx_path (str): Path to a .docx file
+        split_paragraphs (bool, optional): If True, text is split on newlines into a list of paragraphs otherwise, text is returned. Defaults to False.
+    """    
+
+    root = get_xml_root(docx_path)
+
+    text = ""
+    for paragraph in root.findall(".//w:p", namespaces):
+        text += "\n"
+        for elem in paragraph.findall(".//w:t", namespaces):
+            text += elem.text
+
+    if not split_paragraphs:
+        return text
+    else:
+        paragraphs = text.split("\n")
+        paragraphs = [para for para in paragraphs if para != ""] # Remove blank paragraphs
+        return paragraphs
